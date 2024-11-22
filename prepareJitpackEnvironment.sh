81f0c7dcb55ab04045382686d6cc4b1e0e0b8a04
Hassan Wasfy <hassanwasfy7@gmail.com>
2024-11-22 13:04:15 +0200

    update the script for the jitpack io

--- prepareJitpackEnvironment.sh
+++ prepareJitpackEnvironment.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
-# Prepare the JitPack build environment for Maven publication
+
+# Set the file path for the AAR file
+AAR_FILE_PATH="./build/itell-release.aar"
+
+# Check if the file exists
+if [ -f "$AAR_FILE_PATH" ]; then
+    echo "Found AAR file at $AAR_FILE_PATH"
+else
+    echo "AAR file not found at $AAR_FILE_PATH"
+    exit 1
+fi
+
+# Install the AAR file using Maven
 mvn install:install-file \
-  -Dfile=itell-release.aar \
-  -DgroupId=com.github.hassanwasfy \
-  -DartifactId=itell \
-  -Dversion=2.0 \
-  -Dpackaging=aar \
-  -DgeneratePom=true
+    -Dfile=$AAR_FILE_PATH \
+    -DgroupId=com.github.hassanwasfy \
+    -DartifactId=iTell \
+    -Dversion=2.0 \
+    -Dpackaging=aar \
+    -DgeneratePom=true
